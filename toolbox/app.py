3b0852cf6478ae0871efe99abe324c7e5c0367f6
nikhilsanghi <nikhilsanghi6@gmail.com>
2021-06-16 20:39:38 +0530

    Added some comments

--- toolbox/app.py
+++ toolbox/app.py
@@ -6,20 +6,30 @@ import Utils
 import plotly.graph_objects as go
 from PIL import Image
 
-# This will add a homepage pic to the dasghboard
+# Dashboard headline
 st.write("""
 # Cryptocurrency Dashboard Application
 Visually show data on cryptocurrency
 """)
+
+#  Creating some sidebar inputs
 st.sidebar.header("User Input")
+
+# This will add a homepage pic to the dashboard
 image = Image.open(
     "/Users/nikhilsanghi/Documents/Pycharm_Projects/CryptoCurrency/Images/CryptoDashboardPic.png")
-
 st.image(image, use_column_width=True)
+
+# fetching the start_date, end_date and the symbol from user
 start, end, symbol = Utils.get_input()
+
+# depending upon the input porvided by the user, we will fetch the required data
 df = Utils.get_data(symbol, start, end)
+
+# Fetching the cryptocurrency name from the abbreviated form
 crypto_name = Utils.get_crypto_name(symbol)
 
+#Creating a candlestick graph for deatail analysis on daily changes in the prices of currencies
 df = Utils.get_data(symbol, start, end)
 fig = go.Figure(
     data=[
